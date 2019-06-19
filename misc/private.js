798fa332f59c9b932d0d359dcf777cf4f0716ab3
Colin McCarthy <chmccc@gmail.com>
2019-06-19 10:33:05 -0700

    merge misc, add vscode debug

---
+++ misc/private.js
@@ -0,0 +1,7 @@
+// const m = new Mathifier(2) 
+
+// console.log(m.multiply(3).subtract(2).getValue()) // --> 4 AND there must be no way to access 4 except via getValue()
+
+/* Implement an LRU cache with a max capacity of 2 */
+
+/* Group anagrams */
