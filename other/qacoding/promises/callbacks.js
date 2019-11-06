3aae39460b9a5a77bebae93d54b6d9eb31137b08
Colin McCarthy <chmccc@gmail.com>
2019-11-05 22:40:37 -0800

    nov 6 2019

--- other/qacoding/promises/callbacks.js
+++ other/qacoding/promises/callbacks.js
@@ -2,7 +2,7 @@ const { fetchCallback } = require('./mockRequests');
 
 console.log('first cool thing happens!');
 fetchCallback(
-  'http://www.dollarshaveclub.com/products/shave-butter',
+  'https://www.dollarshaveclub.com/products/shave-butter',
   (response) => {
     console.log('product: ', response);
   }
